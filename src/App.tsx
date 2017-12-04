8afd42a042dfee8903eaabbeab4cdc8b717c81ea
Greg Soltis <gsoltis@google.com>
2017-12-04 12:58:54 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import './App.css';
+
+const logo = require('./logo.svg');
+
+class App extends React.Component {
+  buttonClick = () => {
+    console.log('cliced');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <button onClick={this.buttonClick}>Button</button>
+      </div>
+    );
+  }
+}
+
+export default App;
